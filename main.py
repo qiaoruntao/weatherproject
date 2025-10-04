31d9dd5a14d4ff589bccc6d4b064f9ea603e2b5d
qiaoruntao <qiaoruntao01@gmail.com>
2025-10-04 14:45:52 -0400

    Backup

--- main.py
+++ main.py
@@ -1,19 +1,22 @@
 import xarray as xr
 import pandas as pd
+import cfgrib
 
+datasets = cfgrib.open_datasets("data/cfs/flxf2025100218.01.2025100218.grb2")
 # Open dataset (load all variables)
-ds = xr.open_dataset(
-    "data/cfs/flxf2025100218.01.2025100218.grb2",
-    engine="cfgrib",
-)
+# ds = xr.open_dataset(
+#     "data/cfs/flxf2025100218.01.2025100218.grb2",
+#     engine="cfgrib",
+# )
 
 # Collect variable summaries
 records = []
-for var in ds.data_vars:
-    attrs = ds[var].attrs
-    record = {"Variable": var}
-    record.update(attrs)
-    records.append(record)
+for ds in datasets:
+    for var in ds.data_vars:
+        attrs = ds[var].attrs
+        record = {"Variable": var}
+        record.update(attrs)
+        records.append(record)
 
 # Convert to DataFrame for nice display
 df = pd.DataFrame.from_records(records)
