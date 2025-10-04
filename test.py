5e9c86472f1177a97f41768e467392fdb467e06b
qiaoruntao <qiaoruntao01@gmail.com>
2025-10-04 11:56:44 -0400

    Update

--- test.py
+++ test.py
@@ -4,8 +4,8 @@ rows = query_data(
     db_path="grib_index.sqlite",
     start_iso="2025-10-04T00:00:00",
     end_iso="2025-10-04T01:00:00",
-    lon_min_0_360=80, lon_max_0_360=80,
-    lat_min=80, lat_max=80,
+    lon_min_0_360=80, lon_max_0_360=81,
+    lat_min=80, lat_max=81,
     vars_any=["t2m", "prate"],  # optional
     require_all=False,  # optional
     products=["flxf", "ocnf"],  # optional
