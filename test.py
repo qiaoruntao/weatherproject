6c0df5cebdc673d897980c16e17150bd262db89c
qiaoruntao <qiaoruntao01@gmail.com>
2025-10-04 23:46:46 -0400

    Backup

--- test.py
+++ test.py
@@ -1,13 +1,17 @@
 from grib_index import query_data
+from query_file_index import query_func
 
-rows = query_data(
-    start_iso="2025-11-05T00:00:00",
-    end_iso="2025-11-05T01:00:00",
-    lon_min_0_360=80, lon_max_0_360=81,
-    lat_min=80, lat_max=81,
-    vars_any=["t2m", "prate"],  # optional
-    products=["flxf", "ocnf"],  # optional
-)
+#
+# rows = query_data(
+#     start_iso="2025-11-05T00:00:00",
+#     end_iso="2025-11-05T01:00:00",
+#     lon_min_0_360=80, lon_max_0_360=81,
+#     lat_min=80, lat_max=81,
+#     vars_any=["t2m", "prate"],  # optional
+#     products=["flxf", "ocnf"],  # optional
+# )
+
+rows = query_func("2025-11-05T00:00:00", "heightAboveGround", "t2m", (80, 80, 81, 81))
 
 for r in rows[:5]:
     print(r)
---
+++ test2.py
@@ -0,0 +1,3 @@
+from bunk_index import extract_info
+
+extract_info("data/cfs/flxf2025100218.01.2025100218.grb2")
