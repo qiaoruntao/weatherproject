fcf2034b2087bae0c390f84eb86136cb3fba8663
qiaoruntao <qiaoruntao01@gmail.com>
2025-10-05 12:04:02 -0400

    Upgrade API

--- grib_api.py
+++ grib_api.py
@@ -53,7 +53,9 @@ app = FastAPI(title="GRIB Index API", version="1.0.0")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=['*'],
-    allow_methods=['*']
+    allow_methods=['*'],
+    allow_headers=["*"],
+    allow_credentials=True,
 )
 
 
